ce3a9615fe88856a34efe2b68e47c2d86d618830
alfred <1131004907@qq.com>
2026-08-03 16:52:00 +0800

    修正脚本名称

--- al6-run-recipe.sh
+++ al6-run-recipe.sh
@@ -3,5 +3,5 @@ set -e
 
 DEFAULT_TAG="vllm-node-b12x:20260803"
 
-# 调用 run-recipy.sh，优先传入默认的 -t 参数，并附带所有传入该脚本的额外参数
-./run-recipy.sh -t "$DEFAULT_TAG" "$@"
+# 调用 run-recipe.sh，优先传入默认的 -t 参数，并附带所有传入该脚本的额外参数
+./run-recipe.sh -t "$DEFAULT_TAG" "$@"
